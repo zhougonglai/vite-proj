57809636ded42ed1cf4d73c2c359f157c2e0995e
周公来 <zhougonglai@nn.com>
2021-04-14 00:27:22 +0800

    type: update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,20 +3,12 @@ section.flex-1
   .fab(@click="handleQrcode")
     svg.icon(aria-hidden="true")
       use(xlink:href="#icon-qrcode")
-  template
-    .weui-mask(@click="handleQrcode")
-    .weui-half-screen-dialog(
-      :class="{ 'weui-half-screen-dialog_show': qrcode.value }"
-    )
-      .weui-half-screen-dialog__hd
-        .weui-half-screen-dialog__hd__main
-          .weui-flex
-            img(
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII="
-            )
-            | 昵称
-      .weui-half-screen-dialog__bd bd
-      .weui-half-screen-dialog__ft ft
+  .weui-mask(@click="handleQrcode", v-show="qrcode.value")
+  .weui-half-screen-dialog(
+    :class="{ 'weui-half-screen-dialog_show': qrcode.value }"
+  )
+    .weui-half-screen-dialog__bd
+      img(src="../assets/img/index/WechatIMG54.jpeg", alt="qrcode")
   .content
     .weui-cells__title 自我介绍
     .weui-cells
@@ -114,6 +106,22 @@ section.flex-1 {
     font-size: 2.5rem;
     padding: 8px;
     z-index: 5;
+  }
+
+  .avatar {
+    width: 40px;
+    height: 40px;
+    object-fit: cover;
+    object-position: center;
+    border-radius: 4px;
+  }
+
+  .weui-half-screen-dialog {
+    transform: translateY(100%);
+    transition: transform 0.3s;
+    &.weui-half-screen-dialog_show {
+      transform: translateY(0);
+    }
   }
 }
 </style>
